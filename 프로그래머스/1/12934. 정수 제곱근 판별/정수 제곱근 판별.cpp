b343f781a1aefb0e075195fc64934663d73cd682
JIWAN <ham912@naver.com>
2024-10-24 21:35:51 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    
+    long long root = sqrt(n);
+    
+    if(root * root == n) {
+        answer = (root+1) * (root+1);
+    }
+    
+    else {
+        answer = -1;
+    }
+    
+    return answer;
+}
